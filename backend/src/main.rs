b2d4a8317ea2ff4235a36f93315acd4021046652
Filip <filiptarajko@gmail.com>
2023-09-14 20:35:07 +0200

    Connect Axum with MongoDB

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,22 +1,46 @@
 use axum::{
+    extract::State,
     http::{HeaderValue, Method},
     response::Html,
-    routing::get,
-    Router,
+    routing::{get, post},
+    Json, Router,
 };
+use dotenv::dotenv;
+use mongodb::{Client, Collection};
+use serde::{Deserialize, Serialize};
 use std::net::SocketAddr;
 use tower_http::cors::CorsLayer;
 
+#[derive(Debug, Serialize, Deserialize)]
+struct User {
+    #[serde(rename = "_id", skip_serializing_if = "Option::is_none")]
+    pub id: Option<mongodb::bson::oid::ObjectId>,
+    pub is_admin: bool,
+    pub username: String,
+    pub email: String,
+    pub password: String,
+    pub failed_login_attempts: i32,
+}
+
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route(
-        "/check_connection_to_backend",
-        get(check_connection_to_backend).layer(
-            CorsLayer::new()
-                .allow_origin("http://localhost:5173".parse::<HeaderValue>().unwrap())
-                .allow_methods([Method::GET]),
-        ),
-    );
+    dotenv().ok();
+
+    let database_client = mongodb::Client::with_uri_str(std::env::var("DATABASE_URI").unwrap())
+        .await
+        .unwrap();
+
+    let cors_layer: CorsLayer = CorsLayer::new()
+        .allow_origin("http://localhost:5173".parse::<HeaderValue>().unwrap())
+        // it allows POST?
+        .allow_methods([Method::GET]);
+
+    let app = Router::new()
+        .route("/add_test_user", post(add_test_user))
+        .route("/check_mongo", get(check_mongo))
+        .route("/check_backend", get(check_backend))
+        .layer(cors_layer)
+        .with_state(database_client);
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 3001));
 
@@ -26,6 +50,31 @@ async fn main() {
         .unwrap();
 }
 
-async fn check_connection_to_backend() -> Html<&'static str> {
+async fn check_backend() -> Html<&'static str> {
     Html("Hello World!")
+}
+
+async fn check_mongo(State(client): State<Client>) -> Json<u64> {
+    let collection: Collection<User> = client
+        .database(std::env::var("DATABASE_NAME").unwrap().as_str())
+        .collection(std::env::var("COLLECTION_NAME").unwrap().as_str());
+    let estimated_users_count = collection.estimated_document_count(None).await.unwrap();
+    Json(estimated_users_count)
+}
+
+async fn add_test_user(State(client): State<Client>) -> Html<&'static str> {
+    let test_user = User {
+        id: None,
+        is_admin: false,
+        username: "test".to_string(),
+        email: "test@test.test".to_string(),
+        password: "test".to_string(),
+        failed_login_attempts: 0,
+    };
+
+    let collection: Collection<User> = client
+        .database(std::env::var("DATABASE_NAME").unwrap().as_str())
+        .collection(std::env::var("COLLECTION_NAME").unwrap().as_str());
+    collection.insert_one(test_user, None).await.unwrap();
+    Html("Test user added!")
 }
--- frontend/src/lib/DevelopersPlayground.svelte
+++ frontend/src/lib/DevelopersPlayground.svelte
@@ -1,32 +1,93 @@
 <script lang="ts">
 	import axios from 'axios';
-	let status = 'loading...';
+	let backend_status = 'loading...';
+	let mongo_status = 'loading...';
+	let user_count = 'loading...';
 
 	function check_connection_to_backend() {
+		backend_status = 'loading...';
+		user_count = 'loading...';
 		axios
-			.get('http://localhost:3001/check_connection_to_backend')
+			.get('http://localhost:3001/check_backend')
 			.then((response) => {
 				if (response.status === 200) {
-					status = '200';
+					backend_status = '200';
 				} else {
-					status = `${response.status}`;
+					backend_status = `${response.status}`;
 				}
 			})
 			.catch((error) => {
-				status = error.message;
+				backend_status = error.message;
 			});
 	}
 
+	function check_connection_to_backend_and_mongo() {
+		mongo_status = 'loading...';
+		axios
+			.get('http://localhost:3001/check_mongo')
+			.then((response) => {
+				if (response.status === 200) {
+					mongo_status = '200';
+					user_count = response.data;
+				} else {
+					mongo_status = `${response.status}`;
+				}
+			})
+			.catch((error) => {
+				mongo_status = error.message;
+			});
+	}
+
+	function add_test_user() {
+		axios
+			.post('http://localhost:3001/add_test_user')
+			.then((response) => {
+				if (response.status === 200) {
+					mongo_status = '200';
+					user_count = response.data;
+				} else {
+					mongo_status = `${response.status}`;
+				}
+			})
+			.catch((error) => {
+				mongo_status = error.message;
+			});
+	}
+
+	function get_style_variant_by_state(state: string) {
+		if (state === 'loading...') {
+			return 'variant-filled-warning';
+		} else if (state === '200') {
+			return 'variant-filled-success';
+		} else {
+			return 'variant-filled-primary';
+		}
+	}
+
 	check_connection_to_backend();
+	check_connection_to_backend_and_mongo();
 </script>
 
 <h2 class="h2 mt-12">Developer's playground</h2>
-<div class={`card p-6 ${status == '200' ? 'variant-filled-success' : 'variant-filled-primary'}`}>
-	Connection to backend: {status}
+<div class={`card p-6 ${get_style_variant_by_state(backend_status)}`}>
+	Connection to backend: {backend_status}
+</div>
+<div class={`card p-6 ${get_style_variant_by_state(mongo_status)}`}>
+	Connection to mongo: {mongo_status}
+</div>
+<div>
+	users: {user_count}
 </div>
 <button
 	class="btn btn-sm variant-filled"
 	on:click={() => {
+		add_test_user();
+	}}>add test user</button
+>
+<button
+	class="btn btn-sm variant-filled"
+	on:click={() => {
 		check_connection_to_backend();
-	}}>test connection to DB</button
+		check_connection_to_backend_and_mongo();
+	}}>reload data</button
 >
